cb909494f6a50a58c7a894ef043ce4e8a2a6369a
tolle <deuta-tao@outlook.com>
2021-12-14 23:28:02 +0800

    add newsfeed model and admin

--- twitter/settings.py
+++ twitter/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'tweets',
     'friendship',
+    'newsfeeds',
 ]
 REST_FRAMEWORK={
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
